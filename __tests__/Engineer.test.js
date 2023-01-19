5e4f2b06f43039467fcc816c4a2dbf43e344b17b
Saraf Nawar <s.999895831@gmail.com>
2023-01-19 11:00:07 -0500

    engineer test for constructor variables

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,28 @@
+const Engineer = require('..lib/Engineer'); 
+const engineer = new Engineer('saraf', '1', 'sarafnrealtor@gmail.com', 'sophack');
+
+test('test to see if we can get the constructor values for engineer object', () => {
+    expect(engineer.name).toBe('saraf');
+    expect(engineer.id).toBe('1');
+    expect(engineer.email).toBe('sarafnrealtor@gmail.com');
+    expect(engineer.githubUsername).toBe('sophack');    
+});
+
+test('test to see if we can get the name from the getName() method', () => {
+    expect(engineer.getName()).toBe('saraf');
+});
+
+
+test('test to see if we can get the id from the getId() method', () => {
+    expect(engineer.getId()).toBe('1');
+});
+
+test('test to see if we can get the email from the getEmail() method', () => {
+    expect(engineer.getEmail()).toBe('sarafnrealtor@gmail.com');
+
+});
+
+test('test to see if we can get the role from the getRole() method', () => {
+    expect(engineer.getRole()).toBe('Engineer');
+
+});
